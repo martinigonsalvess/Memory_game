073a23e4fbfabf16bb3ae91a58c4f7edf2bd659b
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-04-29 12:52:00 +0200

    function createboard

--- src/index.js
+++ src/index.js
@@ -55,3 +55,11 @@ console.log(cards);
 //put random card in grid
 
 const grid = document.querySelector(".grid");
+
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "src/images/blank.png");
+    card.setAttribute("data-id", i);
+  }
+}
